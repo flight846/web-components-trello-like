639eac774453f935640290cc7f6e9dd248b429bf
yazid <yazid@decision-science.com>
2019-11-12 14:23:50 +0800

    Add trello board

---
+++ public/js/index.js
@@ -0,0 +1,3 @@
+console.log('Appending column...')
+import './trello-board.js';
+
---
+++ public/js/trello-board.js
@@ -0,0 +1,81 @@
+import { API } from './apiService.js';
+import './trello-column.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        :host { display: block; }
+        .trello-board-wrapper,
+        .trello-column-wrapper {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            -webkit-box-orient: horizontal;
+            -webkit-box-direction: normal;
+                -ms-flex-direction: row;
+                    flex-direction: row;
+        }
+
+        .trello-column-wrapper {
+            margin: 0 4px;
+            height: 100%;
+            box-sizing: border-box;
+            vertical-align: top;
+            white-space: nowrap;
+        }
+    </style>
+    <div class="trello-board-wrapper">
+        <div class="trello-column-wrapper"></div>
+        <trello-add-column></trello-add-column>
+    </div>
+`
+class TrelloBoard extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // this.$columnCreator = this.querySelector('trello-add-column');
+        this.$columnsContainer = this.shadowRoot.querySelector('.trello-column-wrapper');
+
+        // this.$columnCreator.addEventListener('addColumn', this.addColumn.bind(this));
+    
+        // initial state
+        this._columns = [];
+    
+        this.fetchData.bind(this);
+        this.fetchData();
+    }
+
+    async fetchData() {
+        const columns = await API.get.columns();
+    
+        this._columns = columns;
+        this._render();
+    }
+    
+    _render() {
+        if (!this.$columnsContainer) return;
+    
+        // redraw everything
+        //
+        // NOTE: poor performance but it should be ok for small boards
+        //       We may run into issues quickly when we'll start
+        //       drag'n'dropping cards.
+        this.$columnsContainer.innerHTML = '';
+    
+        this._columns.forEach(({ id, title }, index) => {
+            const $item = document.createElement('trello-column');
+        
+            $item.setAttribute('id', id);
+            $item.setAttribute('title', title);
+            $item.index = index;
+        
+            // Add event listeners
+        
+            this.$columnsContainer.appendChild($item);
+        });
+    }
+}
+
+window.customElements.define('trello-board', TrelloBoard)
